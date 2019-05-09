9a8b8714261d68f352a3c957793efd3bc9187e45
Jan Carbonell <jcllobet@gmail.com>
2019-05-09 04:55:03 +0200

    added an AND perceptron explained intuitively

--- DeepLearning/Feedforward_Neural_Network/perceptron/perceptron.py
+++ DeepLearning/Feedforward_Neural_Network/perceptron/perceptron.py
@@ -2,6 +2,7 @@ import numpy as np
 
 class Perceptron(object):
 
+    #we initialize the perceptron with a given rate, threshold and number of inputs
     def __init__(self, no_of_inputs, threshold=100, learning_rate=0.01):
         self.threshold = threshold
         self.learning_rate = learning_rate
@@ -20,9 +21,11 @@ class Perceptron(object):
 
     def train(self, training_inputs, labels):
         print("")
-        print("Beginning Training...")
+        print("Beginning Training...", end='')
         for _ in range(self.threshold):
             for inputs, label in zip(training_inputs, labels):
+                print('.', end='')
                 prediction = self.predict(inputs)
                 self.weights[1:] += self.learning_rate * (label - prediction) * inputs
                 self.weights[0] += self.learning_rate * (label - prediction)
+        print("...\nTraining Complete!")
